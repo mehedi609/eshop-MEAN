53eaf78b3564c5311205b2bc2cd98ff0225a5afa
Mehedi Hasan <mehedi609@gmail.com>
2021-09-30 01:13:37 +0600

    implemented order summary APIs

--- backend-ts/src/controllers/order.controller.ts
+++ backend-ts/src/controllers/order.controller.ts
@@ -3,8 +3,8 @@ import { IOrder } from '../interfaces/order';
 import { Order } from '../models/order.model';
 import { IOrderItem } from '../interfaces/order-item';
 import { OrderItem } from '../models/order-item.model';
-import { Document, PopulatedDoc } from 'mongoose';
-import { IUser } from '../interfaces/user';
+import { Product } from '../models/product.model';
+import { IProduct } from '../interfaces/product';
 
 export const getAllOrders = async (req: Request, res: Response) => {
     try {
@@ -60,7 +60,7 @@ export const addOrder = async (req: Request, res: Response) => {
     } = req.body;
 
     try {
-        const orderItemsIds = await Promise.all(
+        const orderItemsIds: string[] = await Promise.all(
             orderItems.map(async (item: IOrderItem) => {
                 const newOrderItem: IOrderItem = new OrderItem({
                     quantity: item.quantity,
@@ -73,6 +73,17 @@ export const addOrder = async (req: Request, res: Response) => {
             })
         );
 
+        const totalPrices: number[] = await Promise.all(
+            orderItems.map(async (item: IOrderItem) => {
+                const product: IProduct = await Product.findById(item.product);
+                return product.price * item.quantity;
+            })
+        );
+
+        const totalPrice: string = totalPrices
+            .reduce((a: number, b: number) => a + b, 0)
+            .toString();
+
         const newOrder: IOrder = new Order({
             orderItems: orderItemsIds,
             shippingAddress1,
@@ -82,6 +93,7 @@ export const addOrder = async (req: Request, res: Response) => {
             country,
             phone,
             status,
+            totalPrice: +Number.parseFloat(totalPrice).toFixed(2),
             user,
         });
 
@@ -94,6 +106,82 @@ export const addOrder = async (req: Request, res: Response) => {
         }
 
         return res.status(201).json(createdOrder);
+    } catch (e) {
+        return res.status(500).json({ success: false, error: e });
+    }
+};
+
+export const updateOrderStatus = async (req: Request, res: Response) => {
+    try {
+        const updatedOrder: IOrder = await Order.findByIdAndUpdate(
+            req.params.id,
+            { status: req.body.status },
+            { new: true }
+        );
+
+        if (!updatedOrder) {
+            return res
+                .status(400)
+                .json({ success: false, message: 'Order cannot be updated!' });
+        }
+
+        return res.status(201).json(updatedOrder);
+    } catch (e) {
+        return res.status(500).json({ success: false, error: e });
+    }
+};
+
+export const removeOrder = async (req: Request, res: Response) => {
+    try {
+        const removedOrder: IOrder = await Order.findByIdAndRemove(
+            req.params.id
+        );
+
+        if (!removedOrder) {
+            return res
+                .status(400)
+                .json({ success: false, message: 'Order cannot be removed!' });
+        }
+
+        await Promise.all(
+            removedOrder.orderItems.map(async (id: string) => {
+                await OrderItem.findByIdAndRemove(id);
+            })
+        );
+
+        return res
+            .status(200)
+            .json({ success: true, message: 'order is deleted!' });
+    } catch (e) {
+        return res.status(500).json({ success: false, error: e });
+    }
+};
+
+export const getTotalSales = async (req: Request, res: Response) => {
+    try {
+        const totalSales = await Order.aggregate([
+            { $group: { _id: null, totalSales: { $sum: '$totalPrice' } } },
+            { $project: { _id: 0, totalSales: 1 } },
+        ]);
+
+        if (!totalSales) {
+            return res.status(400).json({
+                success: false,
+                message: 'Total Sales cannot be generated!',
+            });
+        }
+
+        return res.status(201).json(totalSales.pop());
+    } catch (e) {
+        return res.status(500).json({ success: false, error: e });
+    }
+};
+
+export const getTotalOrderNumber = async (req: Request, res: Response) => {
+    try {
+        const orderCount = await Order.estimatedDocumentCount();
+
+        return res.status(200).json({ orderCount });
     } catch (e) {
         return res.status(500).json({ success: false, error: e });
     }
--- backend-ts/src/routers/order.router.ts
+++ backend-ts/src/routers/order.router.ts
@@ -3,12 +3,19 @@ import {
     addOrder,
     getAllOrders,
     getOrder,
+    getTotalOrderNumber,
+    getTotalSales,
+    removeOrder,
+    updateOrderStatus,
 } from '../controllers/order.controller';
 
 const router = Router();
 
 router.route('/').get(getAllOrders).post(addOrder);
 
-router.route('/:id').get(getOrder);
+router.route('/:id').get(getOrder).put(updateOrderStatus).delete(removeOrder);
+
+router.route('/get/totalsales').get(getTotalSales);
+router.route('/get/count').get(getTotalOrderNumber);
 
 export const orderRouter = router;
